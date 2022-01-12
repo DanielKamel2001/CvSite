452cf4e551200726fe810dbc74518c281a4f70f1
Daniel Kamel <danielgohara@gmail.com>
2022-01-12 17:19:22 -0500

    base html and starting js

---
+++ scroll.js
@@ -0,0 +1,31 @@
+const projects = [
+    {
+        id:0,
+        title: "CV Site",
+        img:"./images/lonePine.png",
+        projectDesc: "This site which I created and hosted to show off some of the things that I have made.",
+        link:"no😎"
+    },
+    {
+        id:1,
+        title: "Reflex Game",
+        img:"./images/reflexSite.png",
+        projectDesc: "A Simple Reflex Game where the player presses a keyboard button witin a certain amount of time to gain points;If the player takes to long or presses the wrong key they lose points. ",
+        link:"danielgohara.com",
+    },
+
+]
+
+
+const projectTitle = document.getElementById("projectTitle")
+const projectDesc = document.getElementById("projectDesc")
+const image = document.getElementById("sample-img")
+const link = document.getElementById("projectLink")
+
+const buttonPrev = document.querySelector("prev")
+const buttonNext = document.querySelector("next")
+
+
+function swap(){
+
+}
